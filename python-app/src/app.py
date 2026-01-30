b17f6244720aa35520c2f906600995a3a224047e
Moses Adelere <moses.adelere@gmail.com>
2026-01-30 19:49:53 +0200

    initial commit

---
+++ python-app/src/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import datetime, socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+
+def details():
+	return jsonify({
+		'message': 'Hello world',
+		'time': datetime.datetime.today().strftime("%I:%M%p on %d-%m-%Y"),
+		'host': socket.gethostname()
+	})
+
+@app.route('/api/v1/health')
+
+def health():
+	return jsonify({
+		'status': 'Up'
+	})
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0")
